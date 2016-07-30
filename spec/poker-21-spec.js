9d7f13466095ea6cc5934bf1b160a57b61b4d010
baixue1016 <1773158403@qq.com>
2016-07-30 12:26:20 +0800

    commit-two

--- spec/poker-21-spec.js
+++ spec/poker-21-spec.js
@@ -1,5 +1,5 @@
 'use strict';
-let {dealString, getPartSum, dealA, getWinner}=require('../src/poker-21.js');
+let {dealString, getPartSum, dealA, getWinner, pokerWin}=require('../src/poker-21.js');
 
 describe('poker-21-Part1', ()=> {
     let String_a = 'A-3-4-J';
@@ -59,18 +59,52 @@ describe('poker-21-Part4', ()=> {
     let sum_a = 18;
     let sum_b = 14;
     fit("should getWinner", ()=> {
-        let result = getWinner(formatted_a, formatted_b, sum_a, sum_b);
+        let result = getWinner(sum_a, sum_b, formatted_a, formatted_b);
         expect(result).toEqual('A is winner');
     });
 });
 
-describe('poker-21', function () {
-    let formatted_a = ['A', '3', '4', 'J'];
-    let formatted_b = ['A', 'A', '2', '10'];
-    let sum_a = 18;
-    let sum_b = 14;
-    fit("text-1", function () {
-        let result = getWinner(sum_a, sum_b, formatted_a, formatted_b);
+
+describe('poker-21', ()=> {
+    fit("get #1", ()=> {
+        let input1 = 'A-3-4-J';
+        let input2 = 'A-A-2-10';
+        let result = pokerWin(input1, input2);
+        expect(result).toEqual('A is winner');
+    });
+
+    fit("get #2", ()=> {
+        let input1 = 'A-2-9-8-J';
+        let input2 = 'J-3';
+        let result = pokerWin(input1, input2);
+        expect(result).toEqual('B is winner');
+    });
+
+    fit("get #3", ()=> {
+        let input1 = 'A-A-A-K-7';
+        let input2 = 'A-J-6-3';
+        let result = pokerWin(input1, input2);
+        expect(result).toEqual('B is winner');
+    });
+
+    fit("get #4", ()=> {
+        let input1 = '5-7-A-Q';
+        let input2 = '8-9-K-Q';
+        let result = pokerWin(input1, input2);
+        expect(result).toEqual('no winner');
+    });
+
+    fit("get #5", ()=> {
+        let input1 = '4-5-A-10';
+        let input2 = 'K-3-2-5';
+        let result = pokerWin(input1, input2);
+        expect(result).toEqual('no winner');
+    });
+
+    fit("get #6", ()=> {
+        let input1 = '2-3-4-5';
+        let input2 = '4-2-7';
+        let result = pokerWin(input1, input2);
         expect(result).toEqual('A is winner');
     });
 });
--- src/poker-21.js
+++ src/poker-21.js
@@ -9,7 +9,7 @@ function getPartSum(formatted) {
     let a = _.chain(formatted).filter(x=> x !== 'A')
         .map(x=> {
             if (x === 'J' || x === 'Q' || x === 'K') {
-                return _.chain(x).replace(x, 10).value();
+                return _.replace(x, x, 10);
             }
             else {
                 return x;
@@ -19,17 +19,17 @@ function getPartSum(formatted) {
 }
 
 function dealA(formatted, partSum) {
-    let a = _.chain(formatted).filter(x=> x === 'A').value();
+    let a = _.filter(formatted, x=> x === 'A');
     let lenA = a.length;
-
+    let sum;
     let sub = 21 - partSum;
     if (sub < 0) {
-        return "gameover";
+        sum = partSum + lenA;
+        return sum;
     } else {
         let sumA;
-        let sum;
         if (_.floor(sub / 11) === 0) {
-            sumA = lenA * 1;
+            sumA = lenA;
             sum = partSum + sumA;
             return sum;
         } else {
@@ -41,35 +41,46 @@ function dealA(formatted, partSum) {
 }
 
 function getWinner(sumA, sumB, formatted_a, formatted_b) {
-    if (sumA < sumB && sumA < 21 && sumB < 21) {
-        return 'B is winner';
-    } else if (sumA > sumB && sumA < 21 && sumB < 21) {
-        console.log('A is winner') ;
+    if (sumA <= 21 && sumB <= 21) {
+        if (sumA < sumB) {
+            return 'B is winner';
+        } else if (sumA > sumB) {
+            return 'A is winner';
+        } else if (sumA === sumB) {
+            let len_a = formatted_a.length;
+            let len_b = formatted_b.length;
+
+            if (len_a === len_b) {
+                return 'no winner';
+            } else if (len_a < len_b) {
+                return 'A is winner';
+            } else {
+                return 'B is winner';
+            }
+        }
     } else if (sumA > 21 && sumB < 21) {
         return 'B is winner';
     } else if (sumA < 21 && sumB > 21) {
         return 'A is winner';
-    } else if (sumA === sumB && sumA < 21 && sumB < 21) {
-        let c = _.chain(formatted_a).filter(x=> x === 'A').value();
-        let len_a = c.length;
-
-        let d = _.chain(formatted_b).filter(x=> x === 'A').value();
-        let len_b = d.length;
-
-        if (len_a === len_b) {
-            return 'no winner';
-        } else if (len_a < len_b) {
-            return 'A is winner';
-        } else {
-            return 'B is winner';
-        }
     } else if (sumA > 21 && sumB > 21) {
         return 'no winner';
     }
 }
+
+function pokerWin(input1, input2) {
+    let formatted1 = dealString(input1);
+    let formatted2 = dealString(input2);
+    let partSum1 = getPartSum(formatted1);
+    let partSum2 = getPartSum(formatted2);
+    let sum1 = dealA(formatted1, partSum1);
+    let sum2 = dealA(formatted2, partSum2);
+    return getWinner(sum1, sum2, formatted1, formatted2);
+}
+
 module.exports = {
     dealString: dealString,
     getPartSum: getPartSum,
     dealA: dealA,
-    getWinner: getWinner
+    getWinner: getWinner,
+    pokerWin: pokerWin
 };
